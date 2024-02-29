d195d6db1c9634f2b3f541cb47a4f9e9f901af9e
Major Zangari <majorzangari@gmail.com>
2024-02-29 02:39:00 -0500

    start pgn parser

---
+++ src/utils/PGNParser.tsx
@@ -0,0 +1,42 @@
+import { readFileSync } from "fs";
+
+class PGNGame {
+    public file: string;
+    private moveData: string[];
+    private boardState: string[][];
+    private whiteTurn: boolean;
+
+    public static readonly defaultBoard = [
+        ["r", "n", "b", "q", "k", "b", "n", "r"],
+        ["p", "p", "p", "p", "p", "p", "p", "p"],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        ["P", "P", "P", "P", "P", "P", "P", "P"],
+        ["R", "N", "B", "Q", "K", "B", "N", "R"],
+    ];
+
+    constructor(file: string) {
+        this.file = file;
+        const fileContent = readFileSync(file, "utf-8");
+
+        this.boardState = [];
+        for (const row of PGNGame.defaultBoard) {
+            this.boardState.push(row.slice());
+        }
+
+        this.whiteTurn = true;
+
+        // split file content into individual lines
+        const lines = fileContent.split(/\r?\n/);
+
+        // remove all data lines and blank lines
+        const filteredLines = lines.filter((line) => {
+            const cleanedLine = line
+                .replace(/\{.*?\}/g, "")
+                .replace(/;.*$/, "");
+            return !line.startsWith("[") && line.trim() != "";
+        });
+    }
+}
