326c196d733b391addaa371b63cb71eae5a21b64
Major Zangari <majorzangari@gmail.com>
2024-03-01 03:39:34 -0500

    continue PGN parser

--- src/utils/PGNParser.tsx
+++ src/utils/PGNParser.tsx
@@ -1,50 +1,111 @@
-import { readFileSync } from "fs";
+import { Chess, DEFAULT_POSITION, PAWN, WHITE } from "chess.js";
+import { gameList } from "../../public/games/gamelist";
 
-class PGNGame {
-  public file: string;
-  private moveData: string[];
-  private currMove: number;
-  private boardState: string[][];
-  private whiteTurn: boolean;
+export interface BoardCoord {
+    x: number;
+    y: number;
+}
 
-  public static readonly defaultBoard = [
-    ["r", "n", "b", "q", "k", "b", "n", "r"],
-    ["p", "p", "p", "p", "p", "p", "p", "p"],
-    [" ", " ", " ", " ", " ", " ", " ", " "],
-    [" ", " ", " ", " ", " ", " ", " ", " "],
-    [" ", " ", " ", " ", " ", " ", " ", " "],
-    [" ", " ", " ", " ", " ", " ", " ", " "],
-    ["P", "P", "P", "P", "P", "P", "P", "P"],
-    ["R", "N", "B", "Q", "K", "B", "N", "R"],
-  ];
+export interface Move {
+    initialPos: BoardCoord;
+    finalPos: BoardCoord;
+    promotion?: string;
+    capture?: BoardCoord;
+}
 
-  constructor(file: string) {
-    this.file = file;
-    const fileContent = readFileSync(file, "utf-8");
+export interface Castle {
+    long: boolean;
+    white: boolean;
+}
 
-    this.boardState = [];
-    for (const row of PGNGame.defaultBoard) {
-      this.boardState.push(row.slice());
+export class PGNGame {
+    private file: string;
+    private gameState: Chess;
+    private moveData: string[];
+
+    constructor() {
+        this.gameState = new Chess(DEFAULT_POSITION);
+
+        const gameIndex = Math.floor(Math.random() * gameList.length);
+        this.file = gameList[gameIndex];
+        this.moveData = [];
     }
 
-    this.whiteTurn = true;
+    // constructor(file: string) {
+    //     this.gameState = new Chess(DEFAULT_POSITION);
 
-    // split file content into individual lines
-    const lines = fileContent.split(/\r?\n/);
+    //     this.file = file;
+    //     this.moveData = this.parseFileData(file);
+    // }
 
-    // remove all data lines and blank lines
-    const filteredLines = lines.filter((line) => {
-      const cleanedLine = line
-        .replace(/\{.*?\}/g, "")
-        .replace(/;.*$/, "")
-        .replace(/\d+\.+/, "");
+    async init() {
+        try {
+            const response = await fetch(this.file);
+            if (!response.ok) {
+                throw new Error("Failed to fetch data");
+            }
+            const fileContent = await response.text();
+            this.moveData = this.parseFileData(fileContent);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
 
-      return !line.startsWith("[") && line.trim() != "";
-    });
+    private parseFileData(fileContent: string): string[] {
+        // split file content into individual lines, regex used to account for windows and posix style new lines
+        const lines = fileContent.split(/\r?\n/);
 
-    this.moveData = filteredLines.flatMap((str) => str.split(/\s+/));
-    this.currMove = 0;
-  }
+        // remove all data lines and blank lines, and recombine into full string
+        const moveSet = lines
+            .filter((line) => !line.startsWith("[") && line.trim() != "")
+            .join(" ");
 
-  getNextmove() {}
+        // remove all comments in braces, remove all semicolon-formatted comments, and remove all move number indicators
+        const cleanedSet = moveSet
+            .replace(/\{.*?\}/g, "")
+            .replace(/;.*$/, "")
+            .replace(/\d+\.+/g, "")
+            .trim();
+
+        return cleanedSet.split(/\s+/).slice(0, -1);
+    }
+
+    public nextMove(): Move | Castle | null {
+        const nextMove = this.moveData.shift();
+        if (nextMove === undefined) {
+            return null;
+        }
+
+        const move = this.gameState.move(nextMove);
+
+        if (nextMove === "O-O") {
+            return { long: false, white: this.gameState.turn() !== WHITE };
+        } else if (nextMove === "O-O-O") {
+            return { long: true, white: this.gameState.turn() !== WHITE };
+        }
+
+        let capturedPiece = move.captured
+            ? this.stringToCoord(move.to)
+            : undefined;
+
+        if (move.flags.includes("e")) {
+            const capturedRank = this.gameState.turn() === WHITE ? 4 : 5;
+            const capturedFile = this.stringToCoord(move.to).x;
+            capturedPiece = { x: capturedRank, y: capturedFile };
+        }
+
+        return {
+            initialPos: this.stringToCoord(move.from),
+            finalPos: this.stringToCoord(move.to),
+            promotion: move.promotion,
+            capture: capturedPiece,
+        };
+    }
+
+    // Converts from algebraic notation to coordinates
+    private stringToCoord(square: string): BoardCoord {
+        const file = "abcdefgh".indexOf(square[0]);
+        const rank = 8 - parseInt(square[1]);
+        return { x: file, y: rank };
+    }
 }
